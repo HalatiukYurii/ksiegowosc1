434fa12bfeae34c9319c1fc7a503c285fafcff4b
Wydrys <karolina.kuczek@onet.pl>
2017-12-14 20:11:52 +0100

    Napisano klasę AdminView

--- src/pl/sdacademy/views/AdminView.java
+++ src/pl/sdacademy/views/AdminView.java
@@ -1,7 +1,14 @@
 package pl.sdacademy.views;
 
+import pl.sdacademy.controllers.AdminController;
+import pl.sdacademy.models.AdminRegistry;
+
 /**
  * Created by marcin on 13.12.2017.
  */
 public class AdminView {
+    AdminController adminController = new AdminController();
+    public void printAdmins(){
+        System.out.println(adminController.getAllAdminLogins());
+    }
 }
