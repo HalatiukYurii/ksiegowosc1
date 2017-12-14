160d7b2181b67afffbd50bbb2fffe9c5e4dbc62b
Owner-PC\Owner <ptrzopek@gmail.com>
2017-12-14 18:15:51 +0100

    set up patryk

--- src/pl/sdacademy/models/AccountantRegistry.java
+++ src/pl/sdacademy/models/AccountantRegistry.java
@@ -1,4 +1,19 @@
 package pl.sdacademy.models;
 
+import java.util.ArrayList;
+
 public class AccountantRegistry {
+    public static AccountantRegistry instance = null;
+
+    public static AdminRegistry getInstance() {
+        if (instance == null) {
+            instance = new AccountantRegistry();
+        }
+        return instance;
+    }
+
+    private ArrayList<Accountant>
+
 }
+
+
