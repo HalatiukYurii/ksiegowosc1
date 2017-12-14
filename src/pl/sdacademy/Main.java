de3d6694b5271c0b2fbbc4c20140546d7ccef367
Owner-PC\Owner <ptrzopek@gmail.com>
2017-12-14 19:00:42 +0100

    #3 done

--- src/pl/sdacademy/Main.java
+++ src/pl/sdacademy/Main.java
@@ -1,7 +1,11 @@
 package pl.sdacademy;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import pl.sdacademy.controllers.CompanyController;
+import pl.sdacademy.exceptions.AccountantNotFoundException;
 import pl.sdacademy.exceptions.AdminNotFoundException;
+import pl.sdacademy.models.Accountant;
+import pl.sdacademy.models.AccountantRegistry;
 import pl.sdacademy.models.Admin;
 import pl.sdacademy.models.AdminRegistry;
 
@@ -12,6 +16,7 @@ public class Main {
     public enum State {
         INIT,
         LOGGING_IN_AS_ADMIN,
+        LOGGING_IN_AS_ACCT,
         LOGGED_IN,
         CREATING_COMPANY,
         EXIT,
@@ -23,16 +28,24 @@ public class Main {
 
         Admin currentAdmin = null;
 
+        Accountant currentAcct = null;
+
         while (state != State.EXIT) {
             switch(state) {
                 case INIT: {
                     System.out.println("Dzień dobry, co chcesz zrobić?");
                     System.out.println(" 1 - zalogować się jako admin");
+                    System.out.println(" 2 - zalogować się jako księgowy");
                     System.out.println(" 0 - wyjść z programu");
 
                     switch (scanner.nextInt()) {
                         case 1:
                             state = State.LOGGING_IN_AS_ADMIN;
+                            scanner.nextLine();
+                            break;
+
+                        case 2:
+                            state = State.LOGGING_IN_AS_ACCT;
                             scanner.nextLine();
                             break;
 
@@ -63,6 +76,26 @@ public class Main {
                         state = State.LOGGED_IN;
 
                     } catch (AdminNotFoundException e) {
+                        System.out.println("Zły login lub hasło");
+                        state = State.INIT;
+                    }
+                    break;
+                }
+
+                case LOGGING_IN_AS_ACCT: {
+                    System.out.println("Podaj login:");
+                    String login = scanner.nextLine();
+
+                    System.out.println("Podaj hasło:");
+                    String password = scanner.nextLine();
+
+                    try {
+                        currentAcct = AccountantRegistry.getInstance().findAccountant(login, password);
+                        System.out.println("Dzień dobry " + currentAcct.getLogin());
+                        state = State.LOGGED_IN;
+                        // FIXME poprawic funkcje ksiegowego po zalogowaniu
+
+                    } catch (AccountantNotFoundException e) {
                         System.out.println("Zły login lub hasło");
                         state = State.INIT;
                     }
@@ -116,6 +149,5 @@ public class Main {
                 }
             }
         }
-        // write your code here
     }
 }
