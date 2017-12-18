6c283647d1a4ead1811176c73a8c3c43a89cd875
Owner-PC\Owner <ptrzopek@gmail.com>
2017-12-18 18:51:43 +0100

    #3 done

--- src/pl/sdacademy/models/Company.java
+++ src/pl/sdacademy/models/Company.java
@@ -1,15 +1,28 @@
 package pl.sdacademy.models;
 
+import java.util.ArrayList;
+
 /**
  * Created by marcin on 13.12.2017.
  */
 public class Company {
+
     private String name;
     private int yearFound;
+    private String nip;
 
-    public Company(String name, int yearFound) {
+    public Company(String name, int yearFound, String nip) {
         this.name = name;
         this.yearFound = yearFound;
+        this.nip = nip;
+    }
+
+    private ArrayList<Company> accountants;
+
+    public void AccountantRegistry() {
+        this.accountants = new ArrayList<>();
+
+        this.accountants.add(new Accountant("jan")); // FIXME
     }
 
     public String getName() {
@@ -18,5 +31,15 @@ public class Company {
 
     public int getYearFound() {
         return yearFound;
+    }
+
+    public String getNip() { return nip; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
     }
 }
