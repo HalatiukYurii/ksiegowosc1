5baba981be94c3ccf18deeeccfa463978665129e
Michal Mytnik <mytnikmichal@gmail.com>
2017-12-22 16:08:45 +0100

    pl part removed

--- src/pl/sdacademy/exceptions/AccountantNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package pl.sdacademy.exceptions;
-
-public class AccountantNotFoundException extends Exception{
-}
--- src/pl/sdacademy/exceptions/IncorrectNipException.java
+++
@@ -1,8 +0,0 @@
-package pl.sdacademy.exceptions;
-
-public class IncorrectNipException extends Throwable {
-    public IncorrectNipException(String message) {
-        super(message);
-    }
-}
-
